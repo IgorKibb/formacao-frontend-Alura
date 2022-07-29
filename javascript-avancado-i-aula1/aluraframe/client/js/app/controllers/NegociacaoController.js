38c19bfb481c55c760bd53683d7e73df32020428
IGOR <igorkibb@hotmail.com>
2022-07-29 13:52:02 -0300

    A ligação entre as ações do usuário e o modelo

---
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,31 @@
+class NegocicaoController {
+   
+   constructor(){
+      let $ = document.querySelector.bind(document); //bind serve para manter a função do querrySElector.
+      this._inputData = $('#data'); //o $ aqui ja está chamando a função do querySelector, mais ou mens igual no jquerry.
+      this._inputQuantidade = $('#quantidade');
+      this._inputValor = $('#valor');
+   }; //evitar de percorrer o DOM inúmeras vezes!!!
+
+   adiciona (event){
+      event.preventDefault();
+
+      //let data = new Data(this._inputData.value.split('-')); // mesma dunção abaixo
+      //let data = new Data(this._inputData.value.replace(/-/g, ','));
+
+      let data = new Data(...
+         this._inputData.value
+         .split('-')
+         .map((item, indice) => item - indice %2) //%significa módulo //map é um callback?
+         //arrow function, saber mais.
+           // if(indice == 1){
+             //  return item - 1;
+            //}
+      ); //spread oparetor ... entende ue o array tem que ser desmembrado.
+      let negociacao = new Negociacao(
+         data,
+         this._inputQuantidade.value,
+         this._inputValor.value
+      );
+   };
+};
