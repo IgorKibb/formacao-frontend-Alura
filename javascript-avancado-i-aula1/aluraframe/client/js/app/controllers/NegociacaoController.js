e34476f5ad758687643b7001e11c6487fc6e189e
IGOR <igorkibb@hotmail.com>
2022-08-12 12:47:50 -0300

    template strings

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,21 +9,22 @@ class NegocicaoController {
 
    adiciona (event){
       event.preventDefault();
+      let helper = new DateHelper();
 
       //let data = new Data(this._inputData.value.split('-')); // mesma dunção abaixo
       //let data = new Data(this._inputData.value.replace(/-/g, ','));
 
-      let data = new Data(...
+     /* let data = new Data(...
          this._inputData.value
          .split('-')
          .map((item, indice) => item - indice %2) //%significa módulo //map é um callback?
          //arrow function, saber mais.
            // if(indice == 1){
              //  return item - 1;
-            //}
-      ); //spread oparetor ... entende ue o array tem que ser desmembrado.
+            //}*
+      );*/ //spread oparetor ... entende ue o array tem que ser desmembrado.
       let negociacao = new Negociacao(
-         data,
+         helper.textoParaData(this._inputData.value),
          this._inputQuantidade.value,
          this._inputValor.value
       );
