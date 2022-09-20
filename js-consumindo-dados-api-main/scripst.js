81d9c045a32d6239358776026d02a22fecad8622
IGOR <igorkibb@hotmail.com>
2022-09-20 13:28:47 -0300

    Criando funções assíncronas

--- js-consumindo-dados-api-main/scripst.js
+++ js-consumindo-dados-api-main/scripst.js
@@ -1,1 +1,26 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/'); //fetch assínocro e obrigatório o API como este.
+async function buscaEndereco(cep){
+   try{
+      var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)//fetch assínocro e obrigatório o API como este.
+      var consultaCEpConvertida = await consultaCEP.json(); //await só é permmitido dentro de uma função async!!
+      if(consultaCEpConvertida.erro){
+         throw Error('CEP não existente!');
+      }
+      return consultaCEpConvertida;
+   }catch(erro){
+
+   }
+};
+
+
+
+//CÓDIGO ANTIGO:
+/*.then(resposta => resposta.json())
+.then(r=>{
+   if(r.erro){
+      throw Error('Esse CEP não existe!')
+   }else
+   console.log(r)
+   })
+   
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluido'));*/ //finally = finalmente, ou seja, para imprimir a conclusão de um processo do qual se foi solicitado.
